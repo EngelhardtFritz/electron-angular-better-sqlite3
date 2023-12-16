991516d8ed3558b43f426a9f11f2661bda53c86b
Fritz Engelhardt <87179608+EngelhardtFritz@users.noreply.github.com>
2023-12-16 18:24:58 +0100

    fix(angular-app): set valid default route

--- workspaces/angular-app/src/app/app.routes.ts
+++ workspaces/angular-app/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Route } from '@angular/router';
 
 export const APP_ROUTES: Array<Route> = [
   {
-    path: '/',
+    path: '',
     loadChildren: () =>
       import('./modules/settings/settings.routes').then(
         (mod) => mod.SETTINGS_ROUTES
